3e59ed77d662c71d0d6b64416edc1d0a35b99260
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-06-11 21:06:38 -0300

    feat: update .gitignore and docker-compose configuration; refactor main test

--- tests/unit/main.test.ts
+++ tests/unit/main.test.ts
@@ -1,12 +1,11 @@
 import {App} from "../../src/main";
 
 describe("Should test the main file", () => {
-    let app: App = new App(3000);
+    let app: App = App.getInstance();
 
   it("should return a new app instance", () => {
     expect(app).toBeDefined();
   });
-
 
 });
 
