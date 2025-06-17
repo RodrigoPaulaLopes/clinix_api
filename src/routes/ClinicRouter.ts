94172db9f0f74adccff0189c1da8914fcce9bfe8
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-06-17 13:17:12 -0300

    feat: implement ClinicValidator for request validation in Clinic routes

--- src/routes/ClinicRouter.ts
+++ src/routes/ClinicRouter.ts
@@ -2,85 +2,37 @@ import { Router } from "express";
 import { celebrate, Joi, Segments } from "celebrate";
 import ClinicController from "../controllers/ClinicController";
 import { DaysAvailability } from "../enums/DaysAvailability";
+import ClincValidator from "../utils/validators/ClinicValidator";
 
 const clinicRouter = Router();
 
 const clinicController = new ClinicController()
 
 
-export const createClinicValidator = celebrate({
-    [Segments.BODY]: Joi.object({
-        name: Joi.string().max(100).required(),
-        cnpj: Joi.string().length(18).required(),
-        phone: Joi.string().max(15).optional(),
-        email: Joi.string().email().optional(),
-        address: Joi.object({
-            street: Joi.string().max(100).required(),
-            neighborhood: Joi.string().max(100).required(),
-            city: Joi.string().max(50).required(),
-            state: Joi.string().length(2).required(),
-            zip_code: Joi.string().length(8).pattern(/^\d+$/).required(),
-            country: Joi.string().max(100).required(),
-            complement: Joi.string().max(100).optional(),
-        }).required(),
-        opening_hours: Joi.string().max(50).optional(),
-        days_open: Joi.array()
-            .items(Joi.string().valid(...Object.values(DaysAvailability)))
-            .optional(),
-        is_active: Joi.boolean().optional(),
-    }),
-});
-
-export const updateClinicValidator = celebrate({
-    [Segments.PARAMS]: Joi.object({
-        id: Joi.string().required()
-    }),
-    [Segments.BODY]: Joi.object({
-        name: Joi.string().max(100).required(),
-        cnpj: Joi.string().length(18).required(),
-        phone: Joi.string().max(15).optional(),
-        email: Joi.string().email().optional(),
-        address: Joi.object({
-            street: Joi.string().max(100).required(),
-            neighborhood: Joi.string().max(100).required(),
-            city: Joi.string().max(50).required(),
-            state: Joi.string().length(2).required(),
-            zip_code: Joi.string().length(8).pattern(/^\d+$/).required(),
-            country: Joi.string().max(100).required(),
-            complement: Joi.string().max(100).optional(),
-        }).required(),
-        opening_hours: Joi.string().max(50).optional(),
-        days_open: Joi.array()
-            .items(Joi.string().valid(...Object.values(DaysAvailability)))
-            .optional(),
-        is_active: Joi.boolean().optional(),
-    }),
-});
-
-
 
 clinicRouter.post(
     "/",
-    createClinicValidator,
+    ClincValidator.create(),
     clinicController.create.bind(clinicController)
 );
 
 clinicRouter.put(
     "/:id",
-    updateClinicValidator,
+    ClincValidator.update(),
     clinicController.update.bind(clinicController)
 );
 
-clinicRouter.get("/", clinicController.findAll.bind(clinicController));
-clinicRouter.get("/:id", clinicController.findById.bind(clinicController));
+clinicRouter.get("/",
+    clinicController.findAll.bind(clinicController)
+);
+clinicRouter.get("/:id",
+    ClincValidator.find(),
+    clinicController.findById.bind(clinicController)
+);
 
 clinicRouter.delete(
     "/:id",
-    celebrate({
-        [Segments.PARAMS]: Joi.object().keys({
-            id: Joi.string().required()
-        })
-    }),
+    ClincValidator.find(),
     clinicController.delete.bind(clinicController)
 );
 
