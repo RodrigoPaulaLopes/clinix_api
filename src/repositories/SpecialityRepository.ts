165bb865e9159489d595b30bb293216f69830c2b
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-06-17 14:57:38 -0300

    feat: implement SpecialityRepository for managing Speciality entities

---
+++ src/repositories/SpecialityRepository.ts
@@ -0,0 +1,38 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../database/data-source";
+import Speciality from "../database/entities/Speciality";
+
+const specialityRepository = AppDataSource.getRepository(Speciality)
+
+export class SpecialityRepository {
+
+    specialityRepository: Repository<Speciality>
+
+    constructor(){
+        this.specialityRepository = specialityRepository
+    }
+
+    async create(speciality: Partial<Speciality>): Promise<Speciality> {
+        const newSpeciality = this.specialityRepository.create(speciality);
+        return await this.specialityRepository.save(newSpeciality);
+    }
+
+    async findAll(): Promise<Speciality[]> {
+        return await this.specialityRepository.find();
+    }
+
+    async findById(id: string): Promise<Speciality | null> {
+        return await this.specialityRepository.findOneBy({ id });
+    }
+
+    async update(id: string, data: Partial<Speciality>): Promise<Speciality | null> {
+        await this.specialityRepository.update(id, data);
+        return await this.findById(id);
+    }
+
+    async delete(id: string): Promise<boolean> {
+        const result = await this.specialityRepository.delete(id);
+        return result.affected !== undefined && result.affected > 0;
+    }
+    
+}
